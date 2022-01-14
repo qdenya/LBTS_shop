5b84c2fa6226fe62cd5652239871fc299ff81571
qdenya <63596498+qdenya@users.noreply.github.com>
2022-01-14 22:40:32 +0100

    Delete api directory

--- api/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$arr1 = json_decode(file_get_contents("https://www.instagram.com/qdenya/?__a=1"), JSON_UNESCAPED_UNICODE); 
-$body = json_encode($arr1, JSON_UNESCAPED_UNICODE);
-$arr = json_decode($body, JSON_UNESCAPED_UNICODE);
-
-print_r($arr);
-
-?>
